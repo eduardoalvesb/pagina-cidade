976791378ecfe2bf0edceba060e1315dcc6cebb2
Eduardo <159261498+eduardoalvesb@users.noreply.github.com>
2024-07-31 10:37:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let lamp=  document.getElementById("imagem")
+let legendas= document.getElementById("legenda")
+function imagem1(){
+
+    lamp.src = '1.png'
+    legendas.innerHTML= 'Vista da cidade de Orlando'
+    
+}
+function imagem2(){
+
+    lamp. src = '2.png'
+    legendas.innerHTML= 'Um dos parques temáticos de Orlando.'
+    
+}
+
+function imagem3(){
+
+    lamp.src = '3.png'
+    legendas.innerHTML= 'Disney'
+}
+function imagem4(){
+
+    lamp. src = '4.png'
+    legendas.innerHTML= 'Lago em Orlando.'
+}
